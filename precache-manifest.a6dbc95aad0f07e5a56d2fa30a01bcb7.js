43e967e102ea21c44ee64574ab2f4b5187c3f523
madnir12 <anfz12001@gmail.com>
2022-11-25 15:32:24 +0500

    Updates

---
+++ precache-manifest.a6dbc95aad0f07e5a56d2fa30a01bcb7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b5a848d838f5d03eafb5393c368754",
+    "url": "/makharij/index.html"
+  },
+  {
+    "revision": "9c569a8b9b3127f4d7fb",
+    "url": "/makharij/static/css/main.c1473c16.chunk.css"
+  },
+  {
+    "revision": "3262708ecfb5afccea69",
+    "url": "/makharij/static/js/2.f5a348ec.chunk.js"
+  },
+  {
+    "revision": "49b611756b7cdcd9ec9914f14c49889c",
+    "url": "/makharij/static/js/2.f5a348ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c569a8b9b3127f4d7fb",
+    "url": "/makharij/static/js/main.f574504f.chunk.js"
+  },
+  {
+    "revision": "6f60958efc4173769031",
+    "url": "/makharij/static/js/runtime-main.13db407d.js"
+  },
+  {
+    "revision": "1d5204e9e6be06cbd29837e4f4f2aa14",
+    "url": "/makharij/static/media/Aslam.1d5204e9.ttf"
+  },
+  {
+    "revision": "642504988927ac898a3b4cff1357d0b0",
+    "url": "/makharij/static/media/Jameel Noori Nastaleeq .64250498.ttf"
+  },
+  {
+    "revision": "093eed4f4091d0792b60b33a1d98448c",
+    "url": "/makharij/static/media/Wrong-answer-sound-effect.093eed4f.mp3"
+  },
+  {
+    "revision": "d0c5f2229fb79adc465123e0629687bb",
+    "url": "/makharij/static/media/correct-answer.d0c5f222.mp3"
+  }
+]);
